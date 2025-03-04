55188776e53229356d2849397f818318e72d866b
xhyasar <hazal-hazal-yasar@hotmail.com>
2025-03-04 11:45:30 +0300

    Add CartContext for managing shopping cart state

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,4 +1,9 @@
+import {useContext} from "react";
+import {CartContext} from "../store/shopping-cart-context.jsx";
+
 export default function Cart({ items, onUpdateItemQuantity }) {
+    const cartCtx = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -7,10 +12,10 @@ export default function Cart({ items, onUpdateItemQuantity }) {
 
   return (
     <div id="cart">
-      {items.length === 0 && <p>No items in cart!</p>}
-      {items.length > 0 && (
+      {cartCtx.items.length === 0 && <p>No items in cart!</p>}
+      {cartCtx.items.length > 0 && (
         <ul id="cart-items">
-          {items.map((item) => {
+          {cartCtx.items.map((item) => {
             const formattedPrice = `$${item.price.toFixed(2)}`;
 
             return (
